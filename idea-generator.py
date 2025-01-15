78a393f2096056c296bcef63cbc6595e25cd8ecc
Thaumatized <veeti.junkkala@gmail.com>
2025-01-15 23:29:39 +0200

    Add categories

--- idea-generator.py
+++ idea-generator.py
@@ -4,4 +4,22 @@ import random
 wordsFile = open("words.json")
 words = json.load(wordsFile)
 
-print(random.choice(words["adjectives"]) + " " + random.choice(words["structures"]))
+categories = []
+for key in words:
+    if key != "adjectives":
+        categories.append(key)
+
+print("Pick a category")
+index = 0
+for key in categories:
+    print(str(index) + ": " + key)
+    index += 1
+
+index = -1
+while index < 0 or index > len(categories):
+    try:
+        index = int(input())
+    except:
+        pass
+
+print(random.choice(words["adjectives"]) + " " + random.choice(words[categories[index]]))
